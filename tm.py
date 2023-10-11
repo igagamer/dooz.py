404281162fb42a1a8bbf5379e4a4a734b5f9da01
igagamer <113548558+igagamer@users.noreply.github.com>
2023-10-11 19:46:13 +0330

    clock

---
+++ tm.py
@@ -0,0 +1,16 @@
+from tkinter import *
+import time
+window = Tk()
+window.title('Clock')
+window.geometry('600x400')
+def myTime():
+    hour = time.strftime('%H')
+    minute = time.strftime('%M')
+    second = time.strftime('%S')
+    myText = hour + ':' + minute + ':' + second
+    myLabel.config(text=myText)
+    myLabel.after(1000, myTime)
+myLabel = Label (window, text="",font=('arial', 72), fg='yellow',bg='red' )
+myLabel.pack()
+myTime()
+window.mainloop()
